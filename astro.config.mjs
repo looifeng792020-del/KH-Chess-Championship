789614112ae7739e988dea21631f16ffac918e85
unknown <looifeng792020@gmail.com>
2026-05-27 16:11:15 +0800

    configure astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://looifeng792020-del.github.io',
+  base: '/KH-Chess-Championship',
+});
