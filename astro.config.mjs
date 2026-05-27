1bcf47a764c662c8aabd1759ebaaeb7de4166797
unknown <looifeng792020@gmail.com>
2026-05-27 19:19:28 +0800

    add cname

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://looifeng792020-del.github.io',
-  base: '/KH-Chess-Championship',
+  site: 'https://kh-chess-championship.abrdns.com',
 });
